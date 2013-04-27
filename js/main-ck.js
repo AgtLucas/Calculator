cb91cfb3e8a46ece43dd391f937d7921bd7d086a
Lucas <metallucas.ds@gmail.com>
2013-04-27 18:13:52 -0300

    JS...

---
+++ js/main-ck.js
@@ -0,0 +1,5 @@
+"use stict";
+
+function calculate() {
+    var e = document.getElementById("amount"), t = document.getElementById("apr"), n = document.getElementById("years"), r = document.getElementById("zipcode"), i = document.getElementById("payment"), s = document.getElementById("total"), o = document.getElementById("totalinterest");
+};
