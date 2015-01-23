33019e5d2d3799f4fcef253734ed1f53ae7fe63f
Lucas <AgtLucas@users.noreply.github.com>
2015-01-22 22:58:07 -0200

    Delete main-ck.js

--- js/main-ck.js
+++
@@ -1,5 +0,0 @@
-"use stict";
-
-function calculate() {
-    var e = document.getElementById("amount"), t = document.getElementById("apr"), n = document.getElementById("years"), r = document.getElementById("zipcode"), i = document.getElementById("payment"), s = document.getElementById("total"), o = document.getElementById("totalinterest");
-};
